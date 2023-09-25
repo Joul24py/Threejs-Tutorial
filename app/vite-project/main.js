5dd4f597c364c61b3161ec2c9207ef4da3e0da70
Joel Alejandro Espinoza Sánchez <alex.esa18@gmail.com>
2023-09-25 00:25:22 -0600

    Added lights to the scene

--- app/vite-project/main.js
+++ app/vite-project/main.js
@@ -13,10 +13,20 @@ camera.position.setZ(30);
 renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true });
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff, 1000);
+pointLight.position.set(20, 0, 0);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+scene.add(lightHelper);
 
 function animate(){
     requestAnimationFrame(animate);
